7174156079f26780cef2b15fc5a9582a3e861de8
Michael Upton <uptonm@wit.edu>
2019-03-08 11:12:21 -0500

    Added in: - User model with BCrypt Hashing - Auth Routes for signup/login - Secured routes with JWT Authorization Bearer Token - Separated App.js and Index.js for better async testing

--- server/app.js
+++ server/app.js
@@ -1,17 +1,32 @@
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
-const router = express.Router();
-require('./routes/routes')(router);
+
+require('./models/user.model');
+require('./services/auth');
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use('/api', router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = require('./routes/auth.routes');
+const apiRoutes = require('./routes/routes');
+const secureRoute = require('./routes/secure.routes');
+
+app.use('/', routes);
+app.use('/api', apiRoutes);
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
+
+// Handle errors
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 
 module.exports = app;
